b5497e85b6ce41da8e5b42822537aa8605dcc8f1
Gabriel Tumbaga <gabrieltumbaga@odachi.local>
2026-03-11 09:28:59 -0700

    added a hue change to make our site purple

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ filter: "hue-rotate(277deg)" }}>
         <head>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
